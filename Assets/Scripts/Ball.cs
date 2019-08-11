a84ced98f170c5d53fdec1dcea17e8cc6b389585
Exatros <exatros@gmail.com>
2019-08-11 11:48:30 +0200

    #10 Launch The Ball

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,16 @@ public class Ball : MonoBehaviour
 {
     // config params
     [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 0f;
+    [SerializeField] float yPush = 15f;
+
+
+
 
 
     //state
     Vector2 paddleToBalVector;
+    bool hasStarted = false;
         
     // Start is called before the first frame update
     void Start()
@@ -19,6 +26,25 @@ public class Ball : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        }
+        
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xPush,yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
     {
         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
         transform.position = paddlePos + paddleToBalVector;
