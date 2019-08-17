1a0b710bc97cdcacc8486b5cb213f44eb851076d
Exatros <exatros@gmail.com>
2019-08-17 13:05:37 +0200

    #31 Prevent Boring Ball Loops

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,8 +8,8 @@ public class Ball : MonoBehaviour
     [SerializeField] Paddle paddle1;
     [SerializeField] float xPush = 0f;
     [SerializeField] float yPush = 10f;
-
     [SerializeField] AudioClip[] ballSounds;
+    [SerializeField] float randomFactor = 0.2f;
 
     //state
     Vector2 paddleToBalVector;
@@ -18,12 +17,14 @@ public class Ball : MonoBehaviour
 
     // Cached component references
     AudioSource myAudioSource;
+    Rigidbody2D myRigidbody2D;
 
     // Start is called before the first frame update
     void Start()
     {
         paddleToBalVector = transform.position - paddle1.transform.position;
         myAudioSource = GetComponent<AudioSource>();
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -54,10 +55,12 @@ public class Ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 velocityTweak = new Vector2(0, Random.Range(0f, randomFactor));
         if (hasStarted)
         {
-            AudioClip clip = ballSounds[UnityEngine.Random.Range(0,ballSounds.Length)];
+            AudioClip clip = ballSounds[Random.Range(0,ballSounds.Length)];
             myAudioSource.PlayOneShot(clip);
+            myRigidbody2D.velocity += velocityTweak;
         }
     }
 }
